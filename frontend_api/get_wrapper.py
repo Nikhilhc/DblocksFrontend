eb87075a82dae623ebf0a61e2bfecd508a199b2c
Nikhilhc <nikilhc@gmail.com>
2022-08-24 18:49:20 +0530

    updated

--- frontend_api/get_wrapper.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from .blog_api import BlogApi
-
-def get_wrapper(request):
-    action = request.GET.get('action')
-    if action=='blog':
-        ret = BlogApi.blog_display(request)
-        return HttpResponse(ret)
--- frontend_api/post_wrapper.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from .blog_api import BlogApi
-
-def post_wrapper(request):
-    action = request.POST.get('action')
-    if action=='blog':
-        ret = BlogApi.blog_display(request)
-        return HttpResponse(ret)
