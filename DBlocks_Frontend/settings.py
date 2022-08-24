43e9b96cc7cd8d8e41f5b458aadc7435cd1b452f
Nikhilhc <nikilhc@gmail.com>
2022-08-24 12:45:21 +0530

    updated

--- DBlocks_Frontend/settings.py
+++ DBlocks_Frontend/settings.py
@@ -122,7 +122,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATIFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
